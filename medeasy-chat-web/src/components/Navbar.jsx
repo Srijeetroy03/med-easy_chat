168aa584747841b701c591f3c0f570e05e3ff42e
Srijeetroy03 <srijeetroy03@gmail.com>
2024-04-11 02:30:25 +0530

    c

---
+++ medeasy-chat-web/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+      <div className='leftNav'>
+        MedEasy
+      </div>
+      <div className="rightNav">
+        <ul>
+          <li>Melly</li>
+          <li>Find Hospital</li>
+          <li>Doctors</li>
+          <li>Blog</li>
+          <li>Login</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
